10b43c151f4db49e2b30325d155f307b9a5ebded
avshir <av.shirinskaya@gmail.com>
2021-08-18 18:58:35 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  if(n < 0) {
+		n = -n;
+  };
+  let res = n.toString().split('').reverse().join('');    
+  //console.log(res);
+  res = +res;
+  //console.log(res);
+  //console.log(typeof res);  
+	return res;
 }
